1f144bf94e41b2803386e3285c383951ae6a15f9
nhimsel <>
2024-09-19 10:50:21 -0500

    task 2 completed + some minor modifications to task 1

--- hw4.cpp
+++ hw4.cpp
@@ -5,9 +5,9 @@ using namespace std;
 int main()
 {
     //Task 1
-    cout << "Please enter something:" << endl;
+    cout << "Please enter a word:" << endl;
     string something;
-    cin >> something;
+    getline(cin, something);
     string working;
     for(int i=something.length()-1; i>=0; i--)
     {
@@ -15,8 +15,21 @@ int main()
     }
     cout << working << endl;
 
+
     //Task 2
-    
+    cout << "\n Please enter a sentence:" << endl;
+    string sentence;
+    getline(cin,sentence);
+    int count=0;
+    for(int i=0; i<sentence.length(); i++)
+    {
+        string temp = sentence.substr(i,1);
+        if(temp=="a"||temp=="e"||temp=="i"||temp=="o"||temp=="u"||temp=="A"||temp=="E"||temp=="I"||temp=="O"||temp=="U")
+        {
+            count++;
+        }
+    }
+    cout << "This has " << count << " vowels.";
 
     return 0;
 }
