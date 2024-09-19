410bb502d5ae86a56c6f2a336828e026c79b218d
nhimsel <>
2024-09-19 11:09:20 -0500

    task 3 completed

--- hw4.cpp
+++ hw4.cpp
@@ -17,19 +17,36 @@ int main()
 
 
     //Task 2
-    cout << "\n Please enter a sentence:" << endl;
+    cout << "Please enter a sentence:" << endl;
     string sentence;
     getline(cin,sentence);
     int count=0;
     for(int i=0; i<sentence.length(); i++)
     {
-        string temp = sentence.substr(i,1);
-        if(temp=="a"||temp=="e"||temp=="i"||temp=="o"||temp=="u"||temp=="A"||temp=="E"||temp=="I"||temp=="O"||temp=="U")
+        char temp = sentence[i];
+        if(temp=='a'||temp=='e'||temp=='i'||temp=='o'||temp=='u'||temp=='A'||temp=='E'||temp=='I'||temp=='O'||temp=='U')
         {
             count++;
         }
     }
-    cout << "This has " << count << " vowels.";
+    cout << "This has " << count << " vowels." << endl;
+
+
+    //Task 3
+    cout << "Please enter a word:" << endl;
+    string word;
+    getline(cin, word);
+    string lowercase;
+    for(int i=0; i<word.length(); i++)
+    {
+        char temp = word[i];
+        if((int)temp>=97&&(int)temp<=122)
+        {
+            temp=char((int)temp-32);
+        }
+        lowercase+=temp;
+    }
+    cout << lowercase << endl;
 
     return 0;
 }
